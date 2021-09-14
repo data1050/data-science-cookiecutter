eb0837511e387dffc2ea95a4f9c6a4d946dba06d
Samuel S. Watson <samuel.s.watson@gmail.com>
2021-09-13 22:48:17 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An example data science project',
+    author='DATA 1050',
+    license='MIT',
+)
